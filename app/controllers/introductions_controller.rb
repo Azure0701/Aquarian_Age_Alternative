faac4a57b095f0277a4467577bc8634689c6a59a
Azure0701 <s.tzw701@gmail.com>
2019-10-01 18:07:35 +0900

    Add introductions

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -4,4 +4,46 @@ class IntroductionsController < ApplicationController
 
   def story
   end
+
+  def play
+  end
+  
+  def play_buystart
+  end
+
+  def play_rule
+  end
+
+  def play_card
+  end
+
+  def play_gamen
+  end
+
+  def play_operation
+  end
+
+  def play_avatar
+  end
+
+  def mode
+  end
+
+  def point
+  end
+
+  def point_skill
+  end
+
+  def point_alteration
+  end
+
+  def point_battlesense
+  end
+
+  def point_technique
+  end
+
+  def point_deckmake
+  end
 end
