f0a3536b389381593d0b39d6ba066fa0d9b433a5
Azure0701 <s.tzw701@gmail.com>
2019-10-02 14:46:58 +0900

    add BASIC_AUTH_PASSWORD for application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
 
   def after_sign_in_path_for(resource)
     redirect_back(fallback_location: root_path)
